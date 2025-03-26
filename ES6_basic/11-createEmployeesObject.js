569ff75c54717022042760242e5f4994eea40b38
sira-djam <b.soileux@gorange.fr>
2025-03-26 14:18:18 +0100

    fichier 11

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,7 +1,5 @@
 export default function createEmployeesObject(departmentName, employees) {
-	// Créer un objet où la clé est le nom du département et la valeur est le tableau des employés
-	const result = {};
-	result[departmentName] = employees;
-
-	return result;
+	return {
+	  [departmentName]: employees,
+	};
   }
