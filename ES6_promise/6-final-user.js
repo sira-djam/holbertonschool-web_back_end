48b9e5125c8663167b4965cad337095ecef8f039
sira-djam <b.soileux@gorange.fr>
2025-04-03 10:41:32 +0200

    modif fichier 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,32 +1,10 @@
-/* eslint-disable */
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(
-    firstName, lastName, fileName) {
-  const user = {
-    status: 'pending',
-    value: '',
-  };
-  const photo = {
-    status: 'pending',
-    value: '',
-  };
-  try {
-    const signup = await signUpUser(firstName, lastName);
-    user.status = 'fulfilled';
-    user.value = signup;
-  } catch (error) {
-    user.status = 'rejected';
-    user.value = error.toString();
-  }
-  try {
-    const upload = await uploadPhoto(fileName);
-    photo.status = 'fulfilled';
-    photo.value = upload;
-  } catch (error) {
-    photo.status = 'rejected';
-    photo.value = error.toString();
-  }
-  return [user, photo];
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const result = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return result.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : `Error: ${result.reason.message}`,
+  }));
 }
