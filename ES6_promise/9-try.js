e31954939b0e3204c99a71a53647017f35cdc53a
sira-djam <b.soileux@gorange.fr>
2025-04-03 10:35:14 +0200

    fichier 0 a 9

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default function guardrail(mathFunction) {
+	const queue = [];
+	try {
+	  queue.push(mathFunction());
+	} catch (error) {
+	  queue.push(error.toString());
+	}
+	queue.push('Guardrail was processed');
+	return queue;
+  }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # holbertonschool-web_back_end
+ES6_promise
